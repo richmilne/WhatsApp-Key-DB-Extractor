60f3003952da6562fe7d3a663ef9f43a7a1e6ac8
Richard Milne <richmilne@users.noreply.github.com>
2022-09-30 13:13:55 +0200

    Started flattening script - judicious use of gotos instead of deeper nesting.

--- WhatsAppKeyDBExtract.bat
+++ WhatsAppKeyDBExtract.bat
@@ -81,133 +81,145 @@ for %%A in (wapath.txt) do if %%~zA==0 (
     echo.
     echo Exiting ...
     echo.
+    goto clean_temp_files
+)
+
+set ORIGINAL_APK=tmp\WhatsAppInstalled-%versionName%.apk
+echo WhatsApp %versionName% installed
+echo.
+if %versionName% equ %LEGACY_VER% goto backup_data
+if not exist %LEGACY_APK% (
+    echo Downloading legacy WhatsApp %LEGACY_VER% to local folder
+    bin\curl.exe -o %LEGACY_APK% %apkfurl%
 ) else (
-    set ORIGINAL_APK=tmp\WhatsAppInstalled-%versionName%.apk
-    echo WhatsApp %versionName% installed
+    echo Found legacy WhatsApp %LEGACY_VER% in local folder
+)
+
+echo.
+if %sdkver% geq 11 (
+    bin\adb.exe shell am force-stop com.whatsapp
+) else (
+    bin\adb.exe shell am kill com.whatsapp
+)
+
+if not exist %ORIGINAL_APK% (
+    echo Backing up installed WhatsApp %versionName%...
+    bin\adb.exe pull %apkpath% %ORIGINAL_APK%
+    echo Backup complete
+) else (
+    echo Found installed WhatsApp %versionName% in local folder.
+)
+
+echo.
+if %sdkver% geq 23 (
+    echo Removing WhatsApp %versionName% skipping data
+    bin\adb.exe shell pm uninstall -k com.whatsapp
+    echo Removal complete
     echo.
-    if %versionName% gtr %LEGACY_VER% (
-        if not exist %LEGACY_APK% (
-            echo Downloading legacy WhatsApp %LEGACY_VER% to local folder
-            bin\curl.exe -o %LEGACY_APK% %apkfurl%
-        ) else (
-            echo Found legacy WhatsApp %LEGACY_VER% in local folder
-        )
+)
 
+echo Installing legacy WhatsApp %LEGACY_VER%
+if %sdkver% geq 17 (
+    bin\adb.exe install -r -d %LEGACY_APK%
+) else (
+    bin\adb.exe install -r %LEGACY_APK%
+)
+
+echo Install complete
+echo.
+
+:backup_data
+if %sdkver% geq 23 (
+    bin\adb.exe backup -f tmp\whatsapp.ab com.whatsapp
+) else (
+    bin\adb.exe backup -f tmp\whatsapp.ab -noapk com.whatsapp
+)
+
+if not exist tmp\whatsapp.ab (
+    echo Couldn't backup WhatsApp data!
+    echo.
+    goto restore_original_version
+)
+
+:decrypt
+set /p password="Please enter your backup password (leave blank for none) and press Enter: "
+echo.
+if "!password!" == "" (
+    java -jar bin\abe.jar unpack tmp\whatsapp.ab tmp\whatsapp.tar
+) else (
+    java -jar bin\abe.jar unpack tmp\whatsapp.ab tmp\whatsapp.tar "!password!"
+)
+
+set db_files=msgstore.db wa.db axolotl.db chatsettings.db
+for %%f in (%db_files%) do (
+    bin\tar.exe xvf tmp\whatsapp.tar -C tmp\ apps/com.whatsapp/db/%%f
+    if exist tmp\apps\com.whatsapp\%%f (
+        echo Extracting %%f...
+        copy /y tmp\apps\com.whatsapp\db\%%f extracted\%%f
         echo.
-        if %sdkver% geq 11 (
-            bin\adb.exe shell am force-stop com.whatsapp
-        ) else (
-            bin\adb.exe shell am kill com.whatsapp
-        )
-
-        if not exist %ORIGINAL_APK% (
-            echo Backing up installed WhatsApp %versionName%...
-            bin\adb.exe pull %apkpath% %ORIGINAL_APK%
-            echo Backup complete
-        ) else (
-            echo Found installed WhatsApp %versionName% in local folder.
-        )
-
-        echo.
-        if %sdkver% geq 23 (
-            echo Removing WhatsApp %versionName% skipping data
-            bin\adb.exe shell pm uninstall -k com.whatsapp
-            echo Removal complete
-            echo.
-        )
-
-        echo Installing legacy WhatsApp %LEGACY_VER%
-        if %sdkver% geq 17 (
-            bin\adb.exe install -r -d %LEGACY_APK%
-        ) else (
-            bin\adb.exe install -r %LEGACY_APK%
-        )
-
-        echo Install complete
-        echo.
-        if %sdkver% geq 23 (
-            bin\adb.exe backup -f tmp\whatsapp.ab com.whatsapp
-        ) else (
-            bin\adb.exe backup -f tmp\whatsapp.ab -noapk com.whatsapp
-        )
-
-        if exist tmp\whatsapp.ab (
-            echo.
-            set /p password="Please enter your backup password (leave blank for none) and press Enter: "
-            echo.
-            if "!password!" == "" (
-                java -jar bin\abe.jar unpack tmp\whatsapp.ab tmp\whatsapp.tar
-            ) else (
-                java -jar bin\abe.jar unpack tmp\whatsapp.ab tmp\whatsapp.tar "!password!"
-            )
-
-            set db_files=msgstore.db wa.db axolotl.db chatsettings.db
-            for %%f in (%db_files%) do (
-                bin\tar.exe xvf tmp\whatsapp.tar -C tmp\ apps/com.whatsapp/db/%%f
-                if exist tmp\apps\com.whatsapp\%%f (
-                    echo Extracting %%f...
-                    copy /y tmp\apps\com.whatsapp\db\%%f extracted\%%f
-                    echo.
-                )
-            )
-
-            bin\tar.exe xvf tmp\whatsapp.tar -C tmp\ apps/com.whatsapp/f/key
-            if exist tmp\apps\com.whatsapp\f\key (
-                echo Extracting whatsapp.cryptkey ...
-                copy /y tmp\apps\com.whatsapp\f\key extracted\whatsapp.cryptkey
-                echo.
-            )
-
-            if exist tmp\apps\com.whatsapp\f\key (
-                echo Pushing cipher key to: %sdpath%/WhatsApp/Databases/.nomedia
-                bin\adb.exe push tmp\apps\com.whatsapp\f\key %sdpath%/WhatsApp/Databases/.nomedia
-                echo.
-            )
-
-            echo Cleaning up temporary files ...
-            set temp_files=waplen.txt sdpath.txt wapath.txt wapver.txt sdkver.txt whatsapp.ab whatsapp.tar
-            for %%f in (%temp_files%) do (
-                if exist tmp\%%f (
-                    echo    Deleting tmp\%%f
-                    del /q tmp\%%f
-                )
-            )
-
-            if exist tmp\apps (
-                echo    Deleting directory tmp\apps
-                rmdir /s /q tmp\apps
-            )
-
-            echo.
-            echo Done
-            echo.
-        ) else (
-            echo Operation failed
-        )
-
-        if exist %ORIGINAL_APK% (
-            echo Restoring WhatsApp %versionName%
-            if %sdkver% geq 17 (
-                bin\adb.exe install -r -d %ORIGINAL_APK%
-            ) else (
-                bin\adb.exe install -r %ORIGINAL_APK%
-            )
-
-            echo.
-            echo Restore complete
-            echo.
-            REM echo Removing WhatsApp %versionName% temporary apk
-            REM del %ORIGINAL_APK% /s /q
-        )
-
-    ) else (
-        echo Operation failed
     )
+)
+
+bin\tar.exe xvf tmp\whatsapp.tar -C tmp\ apps/com.whatsapp/f/key
+if exist tmp\apps\com.whatsapp\f\key (
+    echo Extracting whatsapp.cryptkey ...
+    copy /y tmp\apps\com.whatsapp\f\key extracted\whatsapp.cryptkey
+    echo.
+)
+
+if exist tmp\apps\com.whatsapp\f\key (
+    echo Pushing cipher key to: %sdpath%/WhatsApp/Databases/.nomedia
+    bin\adb.exe push tmp\apps\com.whatsapp\f\key %sdpath%/WhatsApp/Databases/.nomedia
+    echo.
+)
+
+:clean_temp_files
+echo Cleaning up temporary files ...
+set temp_files=waplen.txt sdpath.txt wapath.txt wapver.txt sdkver.txt whatsapp.ab whatsapp.tar
+for %%f in (%temp_files%) do (
+    if exist tmp\%%f (
+        echo    Deleting tmp\%%f
+        del /q tmp\%%f
+    )
+)
+
+if exist tmp\apps (
+    echo    Deleting directory tmp\apps
+    rmdir /s /q tmp\apps
+)
+
+echo.
+echo Done
+echo.
+) else (
+echo Operation failed
+)
+
+:restore_original_version
+if exist %ORIGINAL_APK% (
+echo Restoring WhatsApp %versionName%
+if %sdkver% geq 17 (
+    bin\adb.exe install -r -d %ORIGINAL_APK%
+) else (
+    bin\adb.exe install -r %ORIGINAL_APK%
+)
+
+echo.
+echo Restore complete
+echo.
+REM echo Removing WhatsApp %versionName% temporary apk
+REM del %ORIGINAL_APK% /s /q
+)
+
+) else (
+echo Operation failed
+)
 )
 )
 
 echo.
 echo Operation complete
+
 :end
 echo.
 bin\adb.exe kill-server
